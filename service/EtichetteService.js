02662ae4095d25de9ddc30e5e76f97f7d74b2c21
Kevin Delugan <bisumto@gmail.com>
2022-12-28 23:53:56 +0100

    Rimosso delete utente, richiesta auth per rimozione

--- service/EtichetteService.js
+++ service/EtichetteService.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var utils = require('../utils/writer.js');
+const Etichetta = require('../models/Etichetta');
 
 /**
  * Aggiungi etichetta
@@ -9,17 +11,28 @@
  * returns Etichetta
  **/
 exports.aggiungiEtichetta = function(body) {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = {
-  "descrizione" : "descrizione",
-  "categoria" : "categoria",
-  "nome" : "nome"
-};
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+  return new Promise(async function(resolve, reject) {
+    try {
+      // Aggiorna o inserisci l'etichetta
+      var etichetta = await Etichetta.findOneAndUpdate(
+        {nome: body.nome},
+        body,
+        {upsert: true, new: false, setDefaultsOnInsert: true}
+      ).exec();
+      // Se c'è già, restituisci 200, altrimenti 201
+      if(etichetta) {
+        resolve({
+          "messaggio" : "Etichetta aggiornata",
+          "codice" : 200
+        });
+      } else {
+        resolve(utils.respondWithCode(201, {
+          "messaggio" : "Etichetta aggiunta",
+          "codice" : 201
+        }));
+      }
+    } catch (err) {
+      reject(utils.respondWithCode(500, err));
     }
   });
 }
@@ -32,21 +45,21 @@ exports.aggiungiEtichetta = function(body) {
  * returns List
  **/
 exports.ottieniEtichette = function() {
-  return new Promise(function(resolve, reject) {
-    var examples = {};
-    examples['application/json'] = [ {
-  "descrizione" : "descrizione",
-  "categoria" : "categoria",
-  "nome" : "nome"
-}, {
-  "descrizione" : "descrizione",
-  "categoria" : "categoria",
-  "nome" : "nome"
-} ];
-    if (Object.keys(examples).length > 0) {
-      resolve(examples[Object.keys(examples)[0]]);
-    } else {
-      resolve();
+  return new Promise(async function(resolve, reject) {
+    try {
+      // Aggiorna o inserisci l'etichetta
+      var etichette = await Etichetta.find().exec();
+      // Se ce ne sono, restituisci 200, altrimenti 204
+      if(etichette) {
+        resolve(etichette, 200);
+      } else {
+        resolve({
+          "messaggio" : "Nessuna etichetta trovata",
+          "codice" : 204
+        }, 204);
+      }
+    } catch (err) {
+      reject(err, 500);
     }
   });
 }
