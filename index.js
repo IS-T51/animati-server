0d50999fe7f274a7c7a519866abf2df1e2a26bc2
Carmen <carmen.casulli33@gmail.com>
2023-01-27 13:32:25 +0100

    Separato index in index e app

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 'use strict';
 
-const path = require('path');
+const app = require('./src/app.js');
 const http = require('http');
-const cors = require('cors');
+const database = require('./src/utils/database');
 
-var oas3Tools = require('oas3-tools-cors');
 var serverPort = 8080;
 
-// swaggerRouter configuration
-var options = {
-    routing: {
-        controllers: path.join(__dirname, 'src/controllers')
-    },
-    cors: cors()
-};
 
-var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'src/api/openapi.yaml'), options);
-var app = expressAppConfig.getApp();
-
-// Initialize the Swagger middleware
+//create server
 http.createServer(app).listen(serverPort, function () {
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
 });
 
-const database = require('./src/utils/database');
+//connect to database
 database.connect()
 .then(() => {
     database.createCollections();
 });
+
 
 module.exports = app;
---
+++ src/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const path = require('path');
+const cors = require('cors');
+var oas3Tools = require('oas3-tools-cors');
+
+
+// swaggerRouter configuration
+var options = {
+    routing: {
+        controllers: path.join(__dirname, 'controllers')
+    },
+    cors: cors()
+};
+
+// Initialize the Swagger middleware
+var expressAppConfig = oas3Tools.expressAppConfig(path.join(__dirname, 'api/openapi.yaml'), options);
+var app = expressAppConfig.getApp();
+
+
+module.exports = app;
