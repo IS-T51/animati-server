4b678fcbef9ae56ded3b72195c603e1193add634
Carmen <carmen.casulli33@gmail.com>
2023-02-11 19:09:36 +0100

    Aggiunto errore default 500 in writer

--- src/utils/writer.js
+++ src/utils/writer.js
@@ -1,43 +1,63 @@
-var ResponsePayload = function(code, payload) {
+var ResponsePayload = function (code, payload) {
   this.code = code;
   this.payload = payload;
 }
 
-exports.respondWithCode = function(code, payload) {
+exports.respondWithCode = function (code, payload) {
   return new ResponsePayload(code, payload);
 }
 
-var writeJson = exports.writeJson = function(response, arg1, arg2) {
+var writeJson = exports.writeJson = function (response, arg1, arg2) {
   var code;
   var payload;
 
-  if(arg1 && arg1 instanceof ResponsePayload) {
+  if (arg1 && arg1 instanceof ResponsePayload) {
     writeJson(response, arg1.payload, arg1.code);
     return;
   }
 
-  if(arg2 && Number.isInteger(arg2)) {
+  if (arg2 && Number.isInteger(arg2)) {
     code = arg2;
   }
   else {
-    if(arg1 && Number.isInteger(arg1)) {
+    if (arg1 && Number.isInteger(arg1)) {
       code = arg1;
     }
   }
-  if(code && arg1) {
+  if (code && arg1) {
     payload = arg1;
   }
-  else if(arg1) {
+  else if (arg1) {
     payload = arg1;
   }
 
-  if(!code) {
-    // if no response code given, we default to 200
-    code = 200;
+  if (!code) {
+    if (!(payload instanceof Error))
+      // if no response code was given and no error occurred, we default to 200
+      code = 200;
+    else
+    // if an error occurred we default to 500
+    {
+      console.log(payload);
+      code = 500;
+      payload = {
+        messaggio: "Errore interno",
+        errore: payload.message
+      }
+    }
   }
-  if(typeof payload === 'object') {
+  try {
     payload = JSON.stringify(payload, null, 2);
+  } catch (err) {
+    console.log(err);
+    code = 500;
+    payload = {
+      messaggio: "Errore interno",
+      errore: err.message
+    }
+    payload = JSON.stringify(payload, null, 2);
+  } finally {
+    response.writeHead(code, { 'Content-Type': 'application/json' });
+    response.end(payload);
   }
-  response.writeHead(code, {'Content-Type': 'application/json'});
-  response.end(payload);
 }
