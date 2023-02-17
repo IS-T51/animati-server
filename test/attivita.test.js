66aa244861d091823a3344cf1f7572c109a51ff1
tpanjkovic <tpanjkovic@fbk.eu>
2023-02-17 11:23:46 +0100

    Added tests attività

--- test/attivita.test.js
+++ test/attivita.test.js
@@ -5,7 +5,7 @@ const Attivita = require('../src/models/Attivita');
 const Utente = require('../src/models/Utente');
 const UtenteService = require('../src/service/UtenteService');
 const request = require('supertest');
-//const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 //const { OAuth2Client } = require('google-auth-library');
 //const { google } = require('googleapis');
 //const dotenv = require('dotenv');
@@ -17,8 +17,6 @@ const writer = require('../src/utils/writer');
 const EtichettaSchema = require('../src/schema/Etichetta');
 const serverPort = 8080;
 let server;
-
-jest.mock('../src/service/UtenteService');
 
 beforeAll(() => {
     //create server
@@ -50,6 +48,244 @@ beforeEach(async () => {
     await Utente.deleteMany();
 })
 
+describe('GET /attivita/{id}', () => {
+    test('non esiste', async() => {
+        await request(app)
+            .get('/attivita/63b189dbf6e4763b528548f4')
+            .expect(404)
+    })
+
+    test('non valido', async() => {
+        await request(app)
+            .get('/attivita/id')
+            .expect(400)
+    })
+
+    test('esiste', async() => {
+        var utente = await Utente.findOneAndUpdate(
+            { email: "anonimo1@animati.app" },
+            { email: "anonimo1@animati.app", immagine: "https://picsum.photos/700/400", ruolo: "autenticato" },
+            { upsert: true, new: true }
+        ).exec();
+
+        var attivita = await new Attivita(
+            {
+                numeroSegnalazioni: 0,
+                ultimaModifica: new Date(),
+                mediaValutazioni: 0,
+                banner: "https://picsum.photos/700/400",
+                collegamenti: [
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  },
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  }
+                ],
+                informazioni: {
+                  "etàMax": 1,
+                  "descrizione": "descrizione",
+                  "etàMin": 1,
+                  "durataMin": 1,
+                  "titolo": "titolo",
+                  "durataMax": 1,
+                  "giocatoriPerSquadra": 1,
+                  "numeroSquadre": 1,
+                  "giocatoriMax": 1,
+                  "giocatoriMin": 1
+                },
+                autore: utente._id
+            }
+        )
+        await attivita.save();
+
+        await request(app)
+            .get('/attivita/' + attivita._id)
+            .expect(200)
+    })
+})
+
+describe('PUT /attivita/{id}', () => {
+    test('non autenticato', async() => {
+        await request(app)
+            .put('/attivita/id')
+            .expect(401)
+    })
+
+    test('modifica attivita', async() => {
+        var utente = await Utente.findOneAndUpdate(
+            { email: "anonimo1@animati.app" },
+            { email: "anonimo1@animati.app", immagine: "https://picsum.photos/700/400", ruolo: "autenticato" },
+            { upsert: true, new: true }
+        ).exec();
+        
+        const token = jwt.sign(
+            utente.toObject(),
+            process.env.JWT_SECRET_KEY
+        )
+
+        var attivita = await new Attivita(
+            {
+                numeroSegnalazioni: 0,
+                ultimaModifica: new Date(),
+                mediaValutazioni: 0,
+                banner: "https://picsum.photos/700/400",
+                collegamenti: [
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  },
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  }
+                ],
+                informazioni: {
+                  "etàMax": 1,
+                  "etichette": [
+                    {nome: "proposta", categoria: "categoria", descrizione: "descrizione"}
+                  ],
+                  "descrizione": "descrizione",
+                  "etàMin": 1,
+                  "durataMin": 1,
+                  "titolo": "titolo",
+                  "durataMax": 1,
+                  "giocatoriPerSquadra": 1,
+                  "numeroSquadre": 1,
+                  "giocatoriMax": 1,
+                  "giocatoriMin": 1
+                },
+                autore: utente._id
+            }
+        )
+        await attivita.save();
+
+        var attivita2 = {
+            banner: "https://picsum.photos/700/400",
+            collegamenti: [
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                },
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                }
+            ],
+            informazioni: {
+                "etàMax": 1,
+                "descrizione": "descrizione",
+                "etàMin": 1,
+                "durataMin": 1,
+                "titolo": "titolo",
+                "durataMax": 1,
+                "giocatoriPerSquadra": 1,
+                "numeroSquadre": 1,
+                "giocatoriMax": 1,
+                "giocatoriMin": 1
+            }
+        }
+
+        await request(app)
+            .put('/attivita/' + attivita._id)
+            .set('Authorization', 'Bearer ' + token)
+            .send(attivita2)
+            .expect(200)
+    })
+    
+    test('non autorizzato', async() => {
+        var utente = await Utente.findOneAndUpdate(
+            { email: "anonimo1@animati.app" },
+            { email: "anonimo1@animati.app", immagine: "https://picsum.photos/700/400", ruolo: "autenticato" },
+            { upsert: true, new: true }
+        ).exec();
+        
+        const token = jwt.sign(
+            utente.toObject(),
+            process.env.JWT_SECRET_KEY
+        )
+        
+        var id;
+        if (utente._id == "63b189dbf6e4763b528548f4") {
+            id = "63b189dbf6e4763b528548f3";
+        }
+        else {
+            id = "63b189dbf6e4763b528548f4";
+        }
+
+        var attivita = await new Attivita(
+            {
+                numeroSegnalazioni: 0,
+                ultimaModifica: new Date(),
+                mediaValutazioni: 0,
+                banner: "https://picsum.photos/700/400",
+                collegamenti: [
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  },
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  }
+                ],
+                informazioni: {
+                  "etàMax": 1,
+                  "etichette": [
+                    { nome: "proposta", descrizione: "descrizione", categoria: "categoria" }
+                  ],
+                  "descrizione": "descrizione",
+                  "etàMin": 1,
+                  "durataMin": 1,
+                  "titolo": "titolo",
+                  "durataMax": 1,
+                  "giocatoriPerSquadra": 1,
+                  "numeroSquadre": 1,
+                  "giocatoriMax": 1,
+                  "giocatoriMin": 1
+                },
+                autore: id
+            }
+        )
+        await attivita.save();
+
+        var attivita2 = {
+            banner: "https://picsum.photos/700/400",
+            collegamenti: [
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                },
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                }
+            ],
+            informazioni: {
+                "etàMax": 1,
+                "descrizione": "descrizione",
+                "etàMin": 1,
+                "durataMin": 1,
+                "titolo": "titolo",
+                "durataMax": 1,
+                "giocatoriPerSquadra": 1,
+                "numeroSquadre": 1,
+                "giocatoriMax": 1,
+                "giocatoriMin": 1
+            }
+        }
+
+        await request(app)
+            .put('/attivita/' + attivita._id)
+            .set('Authorization', 'Bearer ' + token)
+            .send(attivita2)
+            .expect(403)
+    })
+    
+})
+
 describe('POST /catalogo', () => {
     test('non autenticato', async () => {
         const response = await request(app).post('/catalogo')
@@ -67,30 +303,76 @@ describe('POST /attivita/{id}/segnalazioni', () => {
 })
 
 describe('GET /catalogo/aggiorna', () => {
-    test()
+    
 })
 
 /*
-        await Utente.findOneAndUpdate(
+var utente = await Utente.findOneAndUpdate(
             { email: "anonimo1@animati.app" },
-            { email: "anonimo1@animati.app", immagine: "https://picsum.photos/700/400", ruolo: "amministratore" },
+            { email: "anonimo1@animati.app", immagine: "https://picsum.photos/700/400", ruolo: "autenticato" },
             { upsert: true, new: true }
         ).exec();
-    
-        UtenteService.getUtente.mockImplementation(req => {
-            return new Promise(function (resolve, reject) {
-                resolve({
-                    email: "anonimo1@animati.app",
-                    immagine: "https://picsum.photos/700/400",
-                    ruolo: "amministratore"
-                })
-            })
-        })
 
-        const response = await request(app)
-            .post('/catalogo')
-            .set('Authorization', 'Bearer token')
-            .send(attivita)
+        var attivita = await new Attivita(
+            {
+                numeroSegnalazioni: 0,
+                ultimaModifica: new Date(),
+                mediaValutazioni: 0,
+                banner: "https://picsum.photos/700/400",
+                collegamenti: [
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  },
+                  {
+                    "link": "http://example.com/aeiou",
+                    "nome": "nome"
+                  }
+                ],
+                informazioni: {
+                  "etàMax": 1,
+                  "descrizione": "descrizione",
+                  "etàMin": 1,
+                  "durataMin": 1,
+                  "titolo": "titolo",
+                  "durataMax": 1,
+                  "giocatoriPerSquadra": 1,
+                  "numeroSquadre": 1,
+                  "giocatoriMax": 1,
+                  "giocatoriMin": 1
+                },
+                autore: utente._id
+            }
+        )
+        await attivita.save();
 
-        expect(response.status).toEqual(403);
+        var attivita2 = {
+            numeroSegnalazioni: 0,
+            ultimaModifica: new Date(),
+            mediaValutazioni: 0,
+            banner: "https://picsum.photos/700/400",
+            collegamenti: [
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                },
+                {
+                "link": "http://example.com/aeiou",
+                "nome": "nome"
+                }
+            ],
+            informazioni: {
+                "etàMax": 1,
+                "descrizione": "descrizione",
+                "etàMin": 1,
+                "durataMin": 1,
+                "titolo": "titolo",
+                "durataMax": 1,
+                "giocatoriPerSquadra": 1,
+                "numeroSquadre": 1,
+                "giocatoriMax": 1,
+                "giocatoriMin": 1
+            },
+            autore: utente._id
+        }
 */
